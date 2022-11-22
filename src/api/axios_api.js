5cd2c53e46c691f2a82363194327d50087f63ffd
Trinity <amoahtnt6@gmail.com>
2022-11-22 00:52:23 +0000

    axios interceptor

--- src/api/axios_api.js
+++ src/api/axios_api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "http://localhost:5000/api/v1/",
+  // url: 'http://localhost:5000/api/v1/',
   timeout: 5000,
 });
 
@@ -10,15 +11,42 @@ axiosInstance.interceptors.response.use(
     return res;
   },
   (err) => {
+    const originalRequest = err.config;
+    const errStatus = err.response.status;
     if (
-      err.config.url.includes("login") ||
+        err.config.url.includes("login") ||
       err.config.url.includes("register") ||
-      err.config.url.includes("verify")
+      err.config.url.includes("verify") ||
+      errStatus !== 401
     ) {
       return Promise.reject(err);
     }
+
     // request for a new tokens from the backend
-    console.log("Play Fuck u");
+    if (errStatus === 403) {
+      // get refreshToken from localStorage
+      const token = localStorage.getItem("user");
+      const { refreshToken } = JSON.parse(token);
+
+      // Making a new request to fetch a new refresh and access tokens
+      if (refreshToken) {
+        axiosInstance
+          .post("student/refreshToken", {
+            token: refreshToken,
+          })
+          .then((e) => {
+            const {
+              data: { accessToken, refreshToken },
+            } = e.data;
+            localStorage.setItem(
+              "user",
+              JSON.stringify({ accessToken, refreshToken })
+            );
+            
+            return axios(originalRequest);
+          });
+      }
+    }
+    return Promise.reject(err);
   }
 );
-
